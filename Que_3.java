1a5390375f8cf500b362eed78cf02e9ef8a5cf29
mHwak <mohakyo123@gmail.com>
2022-12-28 15:48:38 +0530

    done

---
+++ Que_3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Que_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int n=5;
+		
+		for (int i=0;i<=a;i++) {
+			for (int j=0;j<n;j++) {
+				System.out.println(" ");
+			}
+			System.out.println("*");
+		}
+	}
+
+}
